6b68681440db624d5f7bd32ef90cf01004a1703f
luidimso <luidi_mso@outlook.com>
2020-11-18 10:05:58 -0300

    Text Field

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,44 @@ class _HomeState extends State<Home> {
           )
         ],
       ),
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Icon(Icons.person_outline,
+            size: 120,
+            color: Colors.green
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: "Weight",
+              labelStyle: TextStyle(
+                color: Colors.green
+              )
+            ),
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.green,
+              fontSize: 25
+            )
+          ),
+          TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: "Height",
+                  labelStyle: TextStyle(
+                      color: Colors.green
+                  )
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Colors.green,
+                  fontSize: 25
+              )
+          )
+        ],
+      )
     );
   }
 }
